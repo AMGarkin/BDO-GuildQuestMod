a9dce40244f1bd8ef71ce114a299bae17450adc5
Garkin <alda@mujmail.cz>
2017-12-13 22:12:51 +0100

    nothing new

--- updateLangData.cmd
+++ updateLangData.cmd
@@ -27,6 +27,6 @@ GOTO:eof
 :MAIN
 BDO_decrypt.exe ads\languagedata_%1.loc languagedata_%1.txt
 ReplaceLanguage.exe replace_%1.txt languagedata_%1.txt languagedata_%1.new
-bdo_encrypt languagedata_%1.new ads\languagedata_%1.loc
-del languagedata_%1.txt
-del languagedata_%1.new
+BDO_encrypt.exe languagedata_%1.new ads\languagedata_%1.loc
+DEL languagedata_%1.txt
+DEL languagedata_%1.new
