0aeaccd6866344fff5b73d11d278520a8420a60d
Garkin <alda@mujmail.cz>
2018-01-14 17:31:54 +0100

    Add files via upload

--- SEA/updateLangData_SEA.cmd
+++ SEA/updateLangData_SEA.cmd
@@ -1,6 +1,25 @@
 @echo off
-BDO_decrypt.exe ads\languagedata_en.loc languagedata_en.txt
-ReplaceLanguage.exe replace_en.txt languagedata_en.txt languagedata_en.new
-BDO_encrypt.exe languagedata_en.new ads\languagedata_en.loc
-DEL languagedata_en.txt
-DEL languagedata_en.new
+IF [%1]==[en] GOTO EN
+IF [%1]==[EN] GOTO EN
+IF [%1]==[id] GOTO ID
+IF [%1]==[ID] GOTO ID
+
+CALL :MAIN en
+CALL :MAIN id
+GOTO:eof
+
+:EN
+CALL :MAIN en
+GOTO:eof
+
+:ID
+CALL :MAIN id
+GOTO:eof
+
+
+:MAIN
+BDO_decrypt.exe ads\languagedata_%1.loc languagedata_%1.txt
+ReplaceLanguage.exe replace_%1.txt languagedata_%1.txt languagedata_%1.new
+BDO_encrypt.exe languagedata_%1.new ads\languagedata_%1.loc
+DEL languagedata_%1.txt
+DEL languagedata_%1.new
